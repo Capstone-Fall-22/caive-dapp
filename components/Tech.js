0cbb7fe5431c0f384c83be17d0c628bb4c4c5a7b
Kevin Hui <kevinhui98@gmail.com>
2022-11-10 18:10:33 -0500

    starting nft page

--- components/Tech.js
+++ components/Tech.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import styles from '../styles/Tech.module.css'
+const technologies = [
+    {
+        "title": "Web Scraping",
+        "body": "this is web scraping"
+    }, {
+        "title": "Smart Contracts",
+        "body": "this is smart contracts"
+    }, {
+        "title": "backEnd",
+        "body": "this is backend"
+    }, {
+        "title": "frontEnd",
+        "body": "this is frontend"
+    }
+]
 const Tech = () => {
     return (
-        <div>
-
+        <div id="Tech" className={styles["Tech"]}>
+            <h1 className={styles["pageTitle"]}>Tech</h1>
+            <div className={styles["tech-container"]}>
+                {technologies.map((item, index) => {
+                    return (
+                        <div key={index} className={styles["technology"]}>
+                            <h1 className={styles["title"]}>{item.title}</h1>
+                            <h3>{item.body}</h3>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
