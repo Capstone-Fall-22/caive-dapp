db703dc35763b5facf05a0117f6f9da0faece221
Chen Stanilovsky <henrystanilovsky@henrys-air.myfiosgateway.com>
2022-11-14 07:54:55 -0500

    Fix capitalization of imported image:

--- public/socials/socials.js
+++ public/socials/socials.js
@@ -1,7 +1,7 @@
 import Discord from "./Discord_Negative.png"
 import Figma from "./Figma_Negative.png"
 import Github from "./Github_Negative.png"
-import opensea from "./opensea_Negative.png"
+import opensea from "./Opensea_Negative.png"
 
 const socials = {
     Discord,
