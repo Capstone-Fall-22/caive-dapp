7958f677a7b50a587a62b9c5d915fb6afd02e3ea
Kevin Hui <kevinhui98@gmail.com>
2022-11-20 22:59:06 -0500

    adding more style on everything

--- public/icons/icons.js
+++ public/icons/icons.js
@@ -1,8 +1,8 @@
-import About from "./About.jpeg"
-import NFT from "./NFT.jpeg"
-import Tech from "./Tech.jpeg"
+import About from "./About.png"
+import NFT from "./NFT.png"
+import Tech from "./Tech.png"
 import ThreeBars from "./three-bars.png"
-import TimeLine from "./TimeLine.jpeg"
+import TimeLine from "./TimeLine.png"
 
 const Icons = {
     About,
